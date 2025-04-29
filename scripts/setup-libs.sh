9801180d3d70ecbc4410bb1dbd689be7d6da0481
Prasad Subrahmanya <prasad@letsbrew.tech>
2025-04-29 18:57:06 +0200

    fix: Update dependencies and URLs
    - Use JogAmp repository for vecmath.jar
    - Update chart builder dependencies to match required JARs
    - Fix library download URLs
    - Add proper verification for downloads

--- scripts/setup-libs.sh
+++ scripts/setup-libs.sh
@@ -3,11 +3,11 @@
 # Create lib directory if it doesn't exist
 mkdir -p lib
 
-# Download Java 3D libraries from NEA webstart
+# Download Java 3D libraries from NEA webstart and JogAmp
 echo "Downloading Java 3D libraries..."
 wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/j3dcore.jar -O lib/j3d-core.jar
 wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/j3dutils.jar -O lib/j3d-utils.jar
-wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/vecmath.jar -O lib/vecmath.jar
+wget https://jogamp.org/deployment/java3d/1.5.2/jogamp-java3d-1.5.2/jar/vecmath.jar -O lib/vecmath.jar
 
 # Verify downloads
 for jar in lib/j3d-core.jar lib/j3d-utils.jar lib/vecmath.jar; do
@@ -26,8 +26,9 @@ rm -rf jai-1_1_3 jai-1_1_3-lib-linux-amd64.tar.gz
 
 # Download Chart Builder libraries
 echo "Downloading Chart Builder libraries..."
-wget https://github.com/chartbuilder/chartbuilder/releases/download/v1.0.0/chartbuilder-core.jar -O lib/chartbuilder-core.jar
-wget https://github.com/chartbuilder/chartbuilder/releases/download/v1.0.0/chartbuilder-examples.jar -O lib/chartbuilder-examples.jar
+wget https://github.com/chartbuilder/chartbuilder/releases/download/v1.0.0/chartbuilder-data.jar -O lib/chartbuilder-data.jar
+wget https://github.com/chartbuilder/chartbuilder/releases/download/v1.0.0/chartbuilder-parser.jar -O lib/chartbuilder-parser.jar
+wget https://github.com/chartbuilder/chartbuilder/releases/download/v1.0.0/chartbuilder-examples-facialrecognition.jar -O lib/chartbuilder-examples-facialrecognition.jar
 
 # Verify all downloads
 for jar in lib/*.jar; do
