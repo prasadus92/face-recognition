e9dcb0394fc2f6f8e624797d2dd6641aa86856ac
Prasad Subrahmanya <prasad@letsbrew.tech>
2025-04-29 18:41:32 +0200

    fix: Update Java 3D library URLs to NEA webstart
    - Use correct NEA webstart URLs for Java 3D libraries
    - Update version path to 1.5.2
    - Fix library download paths

---
+++ scripts/setup-libs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create lib directory if it doesn't exist
+mkdir -p lib
+
+# Download Java 3D libraries from NEA webstart
+echo "Downloading Java 3D libraries..."
+wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/j3dcore.jar -O lib/j3d-core.jar
+wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/j3dutils.jar -O lib/j3d-utils.jar
+wget https://www.oecd-nea.org/webstart/java3d-1.5.2/j3d/1.5.2/vecmath.jar -O lib/vecmath.jar
+
+# Download JAI libraries
+echo "Downloading JAI libraries..."
+wget https://download.java.net/media/jai/builds/release/1_1_3/jai-1_1_3-lib-linux-amd64.tar.gz
+tar -xzf jai-1_1_3-lib-linux-amd64.tar.gz
+cp jai-1_1_3/lib/*.jar lib/
+rm -rf jai-1_1_3 jai-1_1_3-lib-linux-amd64.tar.gz
+
+echo "Library setup complete!" 
